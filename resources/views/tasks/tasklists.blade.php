bc751cb6d9ac5fb1982310741d39cf53c017dcaf
taro.kirameki <taro.kirameki@techacademy.jp>
2021-10-24 01:44:23 +0000

    user pages

---
+++ resources/views/tasks/tasklists.blade.php
@@ -0,0 +1,23 @@
+@if (count($tasklists) > 0)
+    <ul class="list-unstyled">
+        @foreach ($microposts as $tasklists)
+            <li class="media mb-3">
+                {{-- 投稿の所有者のメールアドレスをもとにGravatarを取得して表示 --}}
+                <img class="mr-2 rounded" src="{{ Gravatar::get($tasklists->user->email, ['size' => 50]) }}" alt="">
+                <div class="media-body">
+                    <div>
+                        {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
+                        {!! link_to_route('users.show', $tasklists->user->name, ['user' => $tasklists->user->id]) !!}
+                        <span class="text-muted">posted at {{ $tasklists->created_at }}</span>
+                    </div>
+                    <div>
+                        {{-- 投稿内容 --}}
+                        <p class="mb-0">{!! nl2br(e($tasklists->content)) !!}</p>
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+    {{-- ページネーションのリンク --}}
+    {{ $microposts->links() }}
+@endif
